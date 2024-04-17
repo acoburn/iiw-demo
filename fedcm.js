a307246e1f85927507ed3f201d97800bee12a966
Aaron Coburn <aaronc@inrupt.com>
2024-04-17 08:02:23 -0700

    formatting

--- fedcm.js
+++ fedcm.js
@@ -16,6 +16,6 @@ import { JOSE } from './authentication.js';
   }).then(credential => {
     const token = credential.token;
     const jwt = JOSE.parse(token);
-    document.getElementById("webid").innerHTML = "WebID: <code>jwt.body.webid</code>";
+    document.getElementById("webid").innerHTML = `WebID: <code>${jwt.body.webid}</code>`;
   });
 })();
